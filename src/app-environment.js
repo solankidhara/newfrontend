2ac539aae4522fd46aeb91683e92ec338c608ad8
solankidhara <120570180+solankidhara@users.noreply.github.com>
2023-06-23 11:54:03 +0530

    Update app-environment.js

--- src/app-environment.js
+++ src/app-environment.js
@@ -1,2 +1,2 @@
-export const Base_URl ="https://mosfrontend-390705.uc.r.appspot.com"
+export const Base_URl ="https://mos-backend-app.uw.r.appspot.com"
 // export const Base_URl ="http://localhost:3000"
